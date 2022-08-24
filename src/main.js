c1c05129cfed3d52755e31807bbe465bbd1da726
Puguh Rismadi <puguh.rismadi@gmail.com>
2022-08-24 12:17:01 +0700

    first commit 2

--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+//import router
 
-createApp(App).mount('#app')
+
+
+//import Bootstrap, Popper, jQuery
+import 'bootstrap/dist/css/bootstrap.css'
+import 'jquery/dist/jquery.min'
+import 'popper.js/dist/popper.min'
+import 'bootstrap/dist/js/bootstrap.min'
+import router from './router'
+
+const app = createApp(App)
+
+//use vue router
+app.use(router)
+
+app.mount('#app')
---
+++ src/views/post/IndexPost.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card border-0 rounded shadow">
+                    <div class="card-body">
+                        <h4>DATA POST</h4>
+                        <hr>
+                        <router-link :to="{name: 'walas.create'}" class="btn btn-md btn-success">TAMBAH WALAS</router-link>
+
+                        <table class="table table-striped table-bordered mt-4">
+                            <thead class="thead-dark">
+                                <tr>
+                                    <th scope="col">Nama Walas</th>
+                                    <th scope="col">Kelas</th>
+                                    <th scope="col">Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr v-for="(post, index) in posts" :key="index">
+                                    <td>{{ post.nama }}</td>
+                                    <td>{{ post.kelas }}</td>
+                                    <td class="text-center">
+                                        <router-link :to="{name: 'walas.edit', params:{id: post.id }}" class="btn btn-sm btn-primary mr-1">EDIT</router-link>
+                                        <button class="btn btn-sm btn-danger ml-1">DELETE</button>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+import { onMounted, ref } from 'vue'
+
+export default {
+
+    setup() {
+
+        //reactive state
+        let posts = ref([])
+
+        //mounted
+        onMounted(() => {
+
+            //get API from Laravel Backend
+            axios.get('http://localhost:8000/api/v1/walas')
+            .then(response => {
+              
+              //assign state posts with response data
+              posts.value = response.data.data
+
+            }).catch(error => {
+                console.log(error.response.data)
+            })
+
+        })
+
+        //return
+        return {
+            posts
+        }
+
+    }
+
+}
+</script>
+
+<style>
+    body{
+        background: lightgray;
+    }
+</style>
